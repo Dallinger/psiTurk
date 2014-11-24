e3935a22236e4569ec0ec9995351b124b4987f50
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-24 15:58:59 -0800

    Install gnureadline only on osx

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 from setuptools import setup
 from psiturk.version import version_number
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     fp.flush()
     fp.close()
 
-    setup(
+    setup_args = dict(
         name = "PsiTurk",
         version = version_number,
         packages = ["psiturk"],
@@ -30,11 +31,6 @@ if __name__ == "__main__":
                 'psiturk-install = psiturk.command_line:process'
             ]
         },
-        setup_requires = [],
-        install_requires = ["argparse", "Flask", "SQLAlchemy", "gunicorn",
-                            "boto>=2.9","cmd2","docopt","gnureadline","requests>=2.2.1","user_agents",
-                            "sh", "fake-factory", "gitpython", "fuzzywuzzy",
-                            "psutil>=1.2.1", "setproctitle"],
         author = "NYU Computation and Cognition Lab",
         author_email = "authors@psiturk.org",
         description = "An open platform for science on Amazon Mechanical Turk",
@@ -43,3 +39,18 @@ if __name__ == "__main__":
         test_suite='test_psiturk'
     )
 
+    # read in requirements.txt for dependencies
+    setup_args['install_requires'] = install_requires = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            req = line.strip()
+            if not req or req.startswith('#'):
+                continue
+            else:
+                install_requires.append(req)
+
+    # add readline only on osx
+    if sys.platform == 'darwin':
+        install_requires.append('gnureadline==6.3.3')
+
+    setup(**setup_args)
